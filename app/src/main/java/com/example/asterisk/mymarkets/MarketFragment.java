718c5c296780fc44b0e1f783a328356c294841fa
NataliiaPrivezentseva <nataliia.privezentseva@gmail.com>
2018-04-13 13:58:39 +0200

    Add an entry to the log

--- app/src/main/java/com/example/asterisk/mymarkets/MarketFragment.java
+++ app/src/main/java/com/example/asterisk/mymarkets/MarketFragment.java
@@ -4,12 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
-public class MarketFragment extends Fragment{
+public class MarketFragment extends Fragment {
 
     /**
      * URL for fetch information about markets
@@ -40,9 +41,12 @@ public class MarketFragment extends Fragment{
 
         // Initiate an {@link AsyncTask} to perform the network request
         FragmentAsyncTask task = new FragmentAsyncTask(listView, getActivity());
-        task.execute(getArguments().getString(MarketFragment.STRING_REQUEST_URL));
-
+        if (getArguments() == null) {
+            Log.e(MarketFragment.class.getSimpleName(),
+                    "There is no information provided through Bundle.");
+        } else {
+            task.execute(getArguments().getString(MarketFragment.STRING_REQUEST_URL));
+        }
         return rootView;
     }
-
 }
